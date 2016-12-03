6e745ab808e089f0526caa62395a3bfa8529b48e
Hernan Malatini <nani93@gmail.com>
2016-12-03 02:33:19 -0300

    REGISTER FILE Se implemento register file segun libro

--- Register_File.v
+++ Register_File.v
@@ -18,14 +18,34 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Register_File(
-    );
+module Register_File(clk, wr_enable3, read_addr1, read_addr2, write_addr3, write_data3, read_data1, read_data2);
 //-------------------------------------------Entradas-----------------------------------------//
+	input clk;
+	input wr_enable3;
+	input [4:0] read_addr1;
+	input [4:0] read_addr2;
+	input [4:0] write_addr3;
+	input [31:0] write_data3;
 //--------------------------------------------Salidas-----------------------------------------//
+	output [31:0] read_data1;
+	output [31:0] read_data2;
 //---------------------------------------------Wires------------------------------------------//
 //-------------------------------------------Registros----------------------------------------//
+	reg [31:0] reg_file[31:0];
+	integer i;
 //-----------------------------------------Inicializacion-------------------------------------//
+	initial
+		for (i = 0; i < 31; i = i + 1)
+			reg_file[i] = 0;
 //--------------------------------------Declaracion de Bloques--------------------------------//
 //--------------------------------------------Logica------------------------------------------//
-
+	always @(posedge clk)
+		begin
+			if (wr_enable3 == 1)
+				reg_file[write_addr3] <= write_data3;
+		end
+	
+	assign read_data1 = (read_addr1 != 0) ? reg_file[read_addr1] : 0;
+	assign read_data2 = (read_addr2 != 0) ? reg_file[read_addr2] : 0;
+	
 endmodule

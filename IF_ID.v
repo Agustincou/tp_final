85a0770511d628a5fa4b4a40bc6eae59bfa399ad
Hernan Malatini <nani93@gmail.com>
2017-08-23 21:57:28 -0300

    Latchs:   - ID->EX   - IF->ID

--- IF_ID.v
+++ IF_ID.v
@@ -18,14 +18,53 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module IF_ID(
-    );
+module IF_ID(clk, reset, clear, stall_ID, programCounter_in, instruction_in, enableDebug, resetDebug, programCounter_out, instruction_out);
 //-------------------------------------------Entradas-----------------------------------------//
+	input clk;
+	input reset;
+	input clear;
+	input stall_ID;
+	input	[7:0] programCounter_in;
+	input	[31:0] instruction_in;
+	//Debug
+	input enableDebug;
+	input resetDebug;
 //--------------------------------------------Salidas-----------------------------------------//
+	output reg [7:0] programCounter_out;
+	output reg [31:0] instruction_out;
 //---------------------------------------------Wires------------------------------------------//
 //-------------------------------------------Registros----------------------------------------//
 //-----------------------------------------Inicializacion-------------------------------------//
+	initial
+		begin
+			programCounter_out = 0;
+			instruction_out = 0;
+		end
 //--------------------------------------Declaracion de Bloques--------------------------------//
 //--------------------------------------------Logica------------------------------------------//
-
+	always @(negedge clock)
+		begin
+			if(reset)
+				begin
+					instruction_out <= 0;
+					programCounter_out <= 0;
+				end
+			else if(resetDebug)
+				begin
+					instruction_out <= 0;
+					programCounter_out <= 0;
+				end
+			else if(~stall_ID && enableDebug)
+				begin
+					if(clear)
+						begin
+							instruction_out <= 0;
+						end
+					else
+						begin
+							instruction_out <= instruction_in;
+						end
+					programCounter_out <= programCounter_in;
+				end
+		end
 endmodule

57a9c8e9cb0cf364ce22252b137f74ce33fd4444
Hernán <nani93@gmail.com>
2017-10-04 20:58:52 -0300

    Added compilador
    In python

---
+++ Compilador/mipsAssembler.py
@@ -0,0 +1,38 @@
+import sys
+from Translator import Translator
+
+if __name__ == '__main__':
+	#Comprobacion de parametros de entrada
+    if(len(sys.argv) < 2):
+        print "ERROR: input file not specified" 
+        exit(1)
+    #Leemos las intrucciones del archivo de entrada
+    tr = Translator()
+    instructionList = list()
+    with open(sys.argv[1],"r") as source:
+        text = source.read()
+        lastLine = text.split("\n")[-1]
+        if "END" not in lastLine.split(";")[0]:
+            print "ERROR: END instruction not detected"
+            exit(1)
+        instructionList = tr.getHexFromAsm(text)
+    
+    #Generamos el archivo de salida
+    outputFileName = "out.coe"
+    if("-o" in sys.argv and (sys.argv.index("-o") < (len(sys.argv)-1))):
+        outputFileName = sys.argv[sys.argv.index("-o")+1]
+    with open(outputFileName, "w+") as outFile:
+        outFile.write('memory_initialization_radix=16;\nmemory_initialization_vector=\n')
+        for line in instructionList:
+            separator = str()
+            if line == instructionList[-1]:
+                separator = ';'
+            else:
+                separator = ',\n'
+            outFile.write(line+separator)
+            
+                
+            
+            
+            
+            
